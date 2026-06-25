618547cc17bbfbdf23c8f09b2d35e2cb36fe3f70
Abdurrehman Masood <abdurrehmanm556@gmail.com>
2026-06-26 01:00:32 +0500

    Configure app for Render deployment

--- app.py
+++ app.py
@@ -74,5 +74,8 @@ def history():
     return render_template("history.html", analyses=analyses)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
